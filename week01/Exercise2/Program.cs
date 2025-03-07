d08b22b2794f9c8c8c8a68b030a14d73b8dcf12f
Delpipi <alexandrepaulkouame@gmail.com>
2025-03-07 21:34:15 +0000

    completed exercice2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter grade percentage(%) ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (grade < 90 && grade >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (grade < 80 && grade >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (grade < 70 && grade >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
     }
 }
