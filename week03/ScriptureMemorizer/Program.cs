a76f5b589e2c36c46b293f0712675f400d5dc0db
Delpipi <alexandrepaulkouame@gmail.com>
2025-03-26 17:44:16 +0000

    completed Creativity and Exceeding Requirements wek04

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,25 +1,55 @@
 using System;
+using System.IO;
+
+
+/* Creativity and Exceeding Requirements:
+    - Program fetch scripture from file "scripture.txt" */
 
 class Program
 {
     static void Main(string[] args)
     {
-        string text = "Trust in the LORD with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.";
-        string book = "Proverbs";
-        int chapter = 3;
-        int verse = 5;
-        int endVerse = 6;
+        string filePath = "scripture.txt";
+        Random random = new Random();
 
-        string answer = "";
-        int numberToHide = 4;
-        bool IsCompletelyHidden = false;
+
+        //Reading all lines into any array
+        string[] lines = File.ReadAllLines(filePath);
+
+        //Random fetch scripture from file
+        int index = random.Next(lines.Length);
+        string line = lines[index];
+
+        //Example of line: "Proverbs 5:4 But her end is bitter as ...."
+        var parts = line.Split(' ', 3); // Split into Book, Chapter, Verse and Text
+        string book = parts[0];
+        string chapterVerse = parts[1];
+        string text = parts[2];
+
+        //Get chapter and verse part
+        var chapterVerseParts = chapterVerse.Split(":");
+        int chapter = int.Parse(chapterVerseParts[0]);
+        int verse = int.Parse(chapterVerseParts[1]);
+
+        /* string text = "Trust in the LORD with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.";
+      string book = "Proverbs";
+      int chapter = 3;
+      int verse = 5;
+      int endVerse = 6; 
 
         //Create Reference
         Reference reference = new Reference(book, chapter, verse, endVerse);
+        */
+
+        //Create Reference
+        Reference reference = new Reference(book, chapter, verse);
 
         //Create Scripture
         Scripture scripture = new Scripture(reference, text);
 
+        bool IsCompletelyHidden;
+        string answer;
+        int numberToHide = 4;
 
         do
         {
