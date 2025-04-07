ba32592585c498c6facf5f9104994f512fe4e41e
Delpipi <alexandrepaulkouame@gmail.com>
2025-04-07 20:58:07 +0000

    completed Homework week05 Activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        string summary = assignment.GetSummary();
+        Console.WriteLine(summary);
+
+        MathAssignment mathAssignment = new MathAssignment("Samuel Bennett", "Multiplication", "7.3", "8-19");
+        string homeworkList = mathAssignment.GetHomeworkList();
+        string summary2 = mathAssignment.GetSummary();
+        Console.WriteLine(summary2);
+        Console.WriteLine(homeworkList);
+
+        WritingAssignment writingAssignment = new WritingAssignment("Samuel Bennett", "Multiplication", "The Causes of World War II by Mary Waters");
+        string title = writingAssignment.GetWritingInformation();
+        string summary3 = writingAssignment.GetSummary();
+        Console.WriteLine(summary3);
+        Console.WriteLine(title);
+
     }
 }
